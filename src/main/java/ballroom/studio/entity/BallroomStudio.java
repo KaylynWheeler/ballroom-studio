1f3f0dfef769388b6f5efa46f2298503d30ba0a2
Kaylyn Wheeler <you@example.com>
2024-10-20 19:43:21 -0500

    update

--- src/main/java/ballroom/studio/entity/BallroomStudio.java
+++ src/main/java/ballroom/studio/entity/BallroomStudio.java
@@ -1,10 +1,15 @@
 package ballroom.studio.entity;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -20,4 +25,9 @@ public class BallroomStudio {
 	private String ballroomStudioZip;
 	private String ballroomStudioPhone;
 	
+	@OneToMany(mappedBy = "amenity", cascade = CascadeType.ALL)
+	private Set <Amenity> amenites  = new HashSet<>();
+	
+	@OneToMany(mappedBy = "instructors", cascade = CascadeType.ALL)
+	private Set <Instructors> instructors  = new HashSet<>();
 }
--- src/main/java/ballroom/studio/entity/Instructors.java
+++ src/main/java/ballroom/studio/entity/Instructors.java
@@ -3,6 +3,7 @@ package ballroom.studio.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -35,9 +38,19 @@ public class Instructors {
 	   inverseJoinColumns = @JoinColumn( name = "students_id"))
 	   private Set<Students> students = new HashSet<>();
 
-	   
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "ballroom_studio_id")
+	private BallroomStudio ballroomStudio; 
 	
+	@OneToMany(mappedBy = "standardstyledancing", cascade = CascadeType.ALL)
+	private Set <StandardStyleDancing> standardstyledances  = new HashSet<>();
 	
+	@OneToMany(mappedBy = "latinstyledancing", cascade = CascadeType.ALL)
+	private Set <LatinStyleDancing> latinstyledances  = new HashSet<>();
 }
+
+
 
 
